2034e3f50e24b70338854d8cf1f51b9ad7b61d0e
Kyle Schaeffer <kyle@oldrivercreative.com>
2015-11-13 10:37:33 -0500

    Bug fixes and improvements
    Security trimmed ribbon, bones object in JS file, various style changes

--- _catalogs/masterpage/bones/script/main.js
+++ _catalogs/masterpage/bones/script/main.js
@@ -1,1 +1,39 @@
 /*! main.js | Place your custom script into this file */
+
+// bones data
+var bones = {
+	
+	// get site
+	site: {
+	    host: window.location.protocol + '//' + window.location.host,
+	    collection: {
+	        url: _spPageContextInfo.siteAbsoluteUrl.replace(/^\/|\/$/g, ''),
+	        relative: _spPageContextInfo.siteServerRelativeUrl.replace(/^\/|\/$/g, '')
+	    },
+	    web: {
+	        url: _spPageContextInfo.webAbsoluteUrl.replace(/^\/|\/$/g, ''),
+	        relative: _spPageContextInfo.webServerRelativeUrl.replace(/^\/|\/$/g, ''),
+	        title: _spPageContextInfo.webTitle,
+	        logo: _spPageContextInfo.webLogoUrl
+	    }
+	},
+
+    // get page
+	page: {
+        id: _spPageContextInfo.pageItemId,
+        physical: _spPageContextInfo.serverRequestPath,
+        title: _spPageContextInfo.webTitle,
+        language: _spPageContextInfo.currentLanguage,
+        editmode: $('#MSOLayout_InDesignMode').val() == '1'
+	},
+	
+	// get user
+	user: {
+	    id: _spPageContextInfo.userId,
+        key: _spPageContextInfo.systemUserKey
+	},
+	
+	// form digest
+	digest: $('#__REQUESTDIGEST').val()
+	
+};
